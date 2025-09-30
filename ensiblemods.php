d3b2dc53846171a63b1aca1fa9e053a87442ba41
Phil <batroff@systopia.de>
2025-09-30 22:04:47 +0200

    Minimal Date Token implementation
    token for date now + 14/21/28 days

--- ensiblemods.php
+++ ensiblemods.php
@@ -3,6 +3,34 @@
 require_once 'ensiblemods.civix.php';
 
 use CRM_Ensiblemods_ExtensionUtil as E;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+const FUTURE_DATE_2_weeks = 14;
+const FUTURE_DATE_3_weeks = 21;
+const FUTURE_DATE_4_weeks =28;
+
+function ensiblemods_civicrm_container(ContainerBuilder $container) {
+  $container->findDefinition('dispatcher')->addMethodCall('addListener', ['civi.token.list', 'ensiblemods_register_tokens'])->setPublic(TRUE);
+  $container->findDefinition('dispatcher')->addMethodCall('addListener', ['civi.token.eval', 'ensiblemods_evaluate_tokens'])->setPublic(TRUE);
+}
+
+
+function ensiblemods_register_tokens(\Civi\Token\Event\TokenRegisterEvent $e) {
+  $e->entity('domain')->register('future' . FUTURE_DATE_2_weeks . 'days',ts('Future date in ' . FUTURE_DATE_2_weeks . ' days' ));
+  $e->entity('domain')->register('future' . FUTURE_DATE_3_weeks . 'days',ts('Future date in ' . FUTURE_DATE_3_weeks . ' days' ));
+  $e->entity('domain')->register('future' . FUTURE_DATE_4_weeks . 'days',ts('Future date in ' . FUTURE_DATE_4_weeks . ' days' ));
+}
+
+
+function ensiblemods_evaluate_tokens(\Civi\Token\Event\TokenValueEvent $e) {
+  foreach ($e->getRows() as $key => $row) {
+    $row->format('text/html');
+    $row->tokens('domain', 'future' . FUTURE_DATE_2_weeks . 'days', date('d.m.Y', strtotime('+' . FUTURE_DATE_2_weeks . 'days')));
+    $row->tokens('domain', 'future' . FUTURE_DATE_3_weeks . 'days', date('d.m.Y', strtotime('+' . FUTURE_DATE_3_weeks . 'days')));
+    $row->tokens('domain', 'future' . FUTURE_DATE_4_weeks . 'days', date('d.m.Y', strtotime('+' . FUTURE_DATE_4_weeks . 'days')));
+  }
+}
+
 
 /**
  * Implements hook_civicrm_config().
